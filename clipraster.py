aa5b74c94356ca9da92fe3f9c0bb5fa23ecfe670
jonasschild <72908189+jonasschild@users.noreply.github.com>
2021-05-03 19:07:42 +0200

    file to clip raster by shp

--- clipraster.py
+++ clipraster.py
@@ -10,3 +10,29 @@ import rtree
 import shapely
 import geopandas as gpd
 
+#Set WS
+
+ws = 'C:/Users/jonas/Desktop/glacier_model_inputdata/Glacier_Outlines'
+
+#Read Shapefile(All Data have to be in the same folder) if not -> Error
+
+ch_glaciers=gpd.read_file(ws+"/"+"SGI_2016_glaciers.shp")
+
+ch_glaciers.head()
+ch_glaciers.columns
+#Read DEM
+DEM = gdal.Open('C:/Users/jonas/Desktop/glacier_model_inputdata/glacier_tsanfleuron.tif')
+
+#Check Projections (EPSG 2056 for ALTI3D)
+ch_glaciers.crs
+
+#select glacier by Name
+myglacier=ch_glaciers.loc[ch_glaciers.name=='Glacier de Tsanfleuron',:]
+myglacier.to_file('myglacier.shp')
+
+
+#Clip
+
+dsClip=gdal.Warp('C:/Users/jonas/Desktop/glacier_model_inputdata/clip_glacier.tif', DEM, cutlineDSName='myglacier.shp', cropToCutline=True, dstNodata=np.nan )
+
+dsClip=None
