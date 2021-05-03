4c48148f11f9cb577c7552fcab0029b9458e0455
jonasschild <72908189+jonasschild@users.noreply.github.com>
2021-05-03 17:07:49 +0200

    added new file for cliping

---
+++ clipraster.py
@@ -0,0 +1,12 @@
+#Imports
+
+from osgeo import osr, gdal
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+import fiona
+import pyproj
+import rtree
+import shapely
+import geopandas as gpd
+
