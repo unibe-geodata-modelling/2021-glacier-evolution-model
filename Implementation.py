7678a89b56d101757bce0e947f0961e959169c73
jonasschild <72908189+jonasschild@users.noreply.github.com>
2021-11-10 17:33:17 +0100

    Finalized Script

--- Implementation.py
+++ Implementation.py
@@ -1,30 +1,16 @@
-#Script for Implementation.
 
 
-
-
-
-#Imports
-
+# Imports
 from osgeo import osr, gdal, ogr
 import numpy as np
-import math
-import matplotlib.pyplot as plt
-import fiona
-import pyproj
-import rtree
-import shapely
-import geopandas as gpd
 import pandas as pd
 
-#Functions
+# Functions
 
-#Read Raster as Array
 def getRasterBand (ds, band=1, access=0):
     band=ds.GetRasterBand(1).ReadAsArray()
     return band
 
-#Substract of the two arrays and copy to new file
 def createRasterFromCopy(dst,ds,data,driverFmt='GTiff'):
     driver=gdal.GetDriverByName(driverFmt)
     outds=driver.CreateCopy(dst,ds,strict=0)
@@ -32,7 +18,6 @@ def createRasterFromCopy(dst,ds,data,driverFmt='GTiff'):
     ds=None
     outds=None
 
-#Normalize dhm and dh
 def normalize_DHM(dhm):
     dhm_n = dhm.copy()
     dhm_n = (dhm.max() - dhm)/(dhm.max() - dhm.min())
@@ -42,8 +27,7 @@ def normalize_dh (dh):
     dh_n = dh/dh.max()
     return dh_n
 
-
-#Workspace: Enter the path of a separate workspace for the implementation
+# Workspace: Enter the path of a separate workspace for the implementation
 
 ws = input('Enter the path of your workspace for the implementation script:')
 
@@ -56,57 +40,45 @@ srs.ImportFromEPSG(21781)
 #Glacier to model.
 glacier = 'Glacier de Tsanfleuron'
 
-
-
-
 #Open and edit files
 
 #Open dhm25
-#dhm25=gdal.Open(ws+'/dhm25_grid_raster.tif')
+dhm25=gdal.Open(ws+'/dhm25_grid_raster.tif')
 
-#Edit DEM25(Reproject and Clip) with the 1998 glacier extend
-#print('edit dhm25')
-#ba_edit_dhm25 = gdal.Warp(ws+'/'+'ba_edit_dhm25.tif', dhm25, dstSRS='EPSG:2056',cutlineDSName=ws+"/98_outline/outline_98_2.shp", cutlineWhere=f"name='{glacier}'", cropToCutline=True, dstNodata=np.nan )
-ba_edit_dhm25 = gdal.Open(ws+'/'+'ba_edit_dhm25.tif')
-
+#Edit dhm25(Reproject and Clip) with the 1998 glacier extend
+ba_edit_dhm25 = gdal.Warp(ws+'/'+'ba_edit_dhm25.tif', dhm25, dstSRS='EPSG:2056',cutlineDSName=ws+"/98_outline/outline_98_2.shp", cutlineWhere=f"name='{glacier}'", cropToCutline=True, dstNodata=np.nan )
 
 #Open and Edit ALTI3D(Resample and Clip) with the 1998 glacier extend
-#print('edit and open swissALTI3D')
-#alti3d = gdal.Open(ws+'/glacier_tsanfleuron.tif')
-#transform = dhm25.GetGeoTransform()
-#ba_edit_alti3d =gdal.Warp(ws +'/ba_edit_glacier_tsanfleuron.tif', alti3d, xRes=transform[1], yRes=transform[1], resampleAlg="bilinear", cutlineDSName=ws + "/98_outline/outline_98_2.shp", cutlineWhere=f"name='{glacier}'", cropToCutline=True, dstNodata=np.nan)
-ba_edit_alti3d =gdal.Open(ws + '/ba_edit_glacier_tsanfleuron.tif')
+alti3d = gdal.Open(ws+'/glacier_tsanfleuron.tif')
+transform = dhm25.GetGeoTransform()
+ba_edit_alti3d =gdal.Warp(ws +'/ba_edit_glacier_tsanfleuron.tif', alti3d, xRes=transform[1], yRes=transform[1], resampleAlg="bilinear", cutlineDSName=ws + "/98_outline/outline_98_2.shp", cutlineWhere=f"name='{glacier}'", cropToCutline=True, dstNodata=np.nan)
 
 #Open the parametrication .txt file
 parametrication = pd.read_csv(ws+'/deltaH_Glacier de Tsanfleuron.txt', sep=';')
 
-
 #Open the Edit Alti3D file with the 2016 glacier extend
-edit_alti3d = gdal.Open(ws + f'/edit_{FileName}')
-
+edit_alti3d = gdal.Open(ws +'/edit_glacier_tsanfleuron.tif')
 
 #Open and edit the glacier bed file
 glacier_bed = gdal.Open(ws + '\GlacierBed.tif')
-edit_glacier_bed =gdal.Warp(ws +f'/glacier_bed_{FileName}', glacier_bed, xRes=25, yRes=25, resampleAlg="bilinear", cutlineDSName=ws + "/glacier_outlines/SGI_2016_glaciers.shp", cutlineWhere=f"name='{glacier}'", cropToCutline=True, dstNodata=np.nan)
-edit_glacier_bed =gdal.Open(ws + f'/glacier_bed_{FileName}')
+edit_glacier_bed =gdal.Warp(ws +'/glacier_bed_gltsf.tif', glacier_bed, xRes=25, yRes=25, resampleAlg="bilinear", cutlineDSName=ws + "/glacier_outlines/SGI_2016_glaciers.shp", cutlineWhere=f"name='{glacier}'", cropToCutline=True, dstNodata=np.nan)
 
 #########################################################Calculating Geodetic Mass Balance Change (Ba) according to Fischer M. et al. 2015##############################################
 
 
 #Calculate volume change from 1998 to 2016
 
-#NewFile = ws+'/'+f'ba_substract_{FileName}'
+NewFile = ws+'/'+ 'ba_substract_glacier_tsanfleuron.tif'
 
 #Calculate Diffrence and write to a new Tiff
 
+Old=getRasterBand(ba_edit_dhm25)
+New=getRasterBand(ba_edit_alti3d)
+Diff=Old-New
 
-#Old=getRasterBand(ba_edit_dhm25)
-#New=getRasterBand(ba_edit_alti3d)
-#Diff=Old-New
+createRasterFromCopy(NewFile, ba_edit_alti3d, Diff)
 
-#createRasterFromCopy(NewFile, ba_edit_alti3d, Diff)
-
-ba_substract=gdal.Open(ws+'/'+f'ba_substract_{FileName}')
+ba_substract=gdal.Open(ws+'/'+'ba_substract_glacier_tsanfleuron.tif')
 
 #Read as Array
 ba_substractArray = ba_substract.GetRasterBand(1).ReadAsArray()
@@ -121,7 +93,7 @@ ba_masked_alti3D= np.ma.masked_invalid(ba_alti3DArray)
 ba_multimask = np.ma.mask_or(np.ma.getmask(ba_masked_substract), np.ma.getmask(ba_masked_alti3D))
 
 ba_substract_Final_Mask=np.ma.array(ba_substractArray, mask=ba_multimask, fill_value=np.nan ).compressed()
-ba_alti3D_Final_Mask=np.ma.array(ba_alti3DArray, mask=ba_multimask,fill_value=np.nan).compressed()
+ba_alti3D_Final_Mask=np.ma.array(ba_alti3DArray, mask=ba_multimask, fill_value=np.nan).compressed()
 
 #Make Dataframe dh/DHM
 ba_df =  pd.DataFrame(np.column_stack([ba_substract_Final_Mask, ba_alti3D_Final_Mask]), columns=['dh','DHM'])
@@ -172,7 +144,6 @@ BA = (dv * fdv) / (A * dt)
 
 #Band Area * normalized elevation range (Ba in m.w.a/yr is convertet in to m with density of water (997) and the area.
 
-((parametrication.iloc[:,2] * parametrication.iloc[:,6]).sum())* 920
 
 fs= (BA* 997 * 2442977) / (((parametrication.iloc[:,2] * parametrication.iloc[:,6]).sum())* 920)
 
@@ -186,45 +157,7 @@ alti3DArray = edit_alti3d.ReadAsArray()
 edit_glacier_bed_array = edit_glacier_bed.ReadAsArray()
 
 
-
-
-#####Update alti 3d array band by band with dh# and compare with glacier bed!!!!!
-
-'''''Band1 = np.where((bins[0] <= alti3DArray) & (alti3DArray >= bins[1]),alti3DArray, alti3DArray+fs*parametrication.iloc[0,1])
-Band2 = np.where(alti3DArray >= bins[2],Band1, alti3DArray+fs*parametrication.iloc[1,1])
-Band3 = np.where(alti3DArray >= bins[3],Band2, alti3DArray+fs*parametrication.iloc[2,1])
-Band4 = np.where(alti3DArray >= bins[4],Band3, alti3DArray+fs*parametrication.iloc[3,1])
-Band5 = np.where(alti3DArray >= bins[5],Band4, alti3DArray+fs*parametrication.iloc[4,1])
-Band6 = np.where(alti3DArray >= bins[6],Band5, alti3DArray+fs*parametrication.iloc[5,1])
-Band7 = np.where(alti3DArray >= bins[7],Band6, alti3DArray+fs*parametrication.iloc[6,1])
-Band8 = np.where(alti3DArray >= bins[8],Band7, alti3DArray+fs*parametrication.iloc[7,1])
-Band9 = np.where(alti3DArray >= bins[9],Band8, alti3DArray+fs*parametrication.iloc[8,1])
-Band10 = np.where(alti3DArray >=bins[10],Band9, alti3DArray+fs*parametrication.iloc[9,1])
-Band11= np.where(alti3DArray >= bins[11],Band10, alti3DArray+fs*parametrication.iloc[10,1])
-Band12= np.where(alti3DArray >= bins[12],Band11, alti3DArray+fs*parametrication.iloc[11,1])
-Band13= np.where(alti3DArray >= bins[13],Band12, alti3DArray+fs*parametrication.iloc[12,1])
-Band14= np.where(alti3DArray >= bins[14],Band13, alti3DArray+fs*parametrication.iloc[13,1])
-Band15 = np.where(alti3DArray >=bins[15],Band14, alti3DArray+fs*parametrication.iloc[14,1])
-Band16 = np.where(alti3DArray >=bins[16],Band15, alti3DArray+fs*parametrication.iloc[15,1])
-Band17 = np.where(alti3DArray >=bins[17],Band16, alti3DArray+fs*parametrication.iloc[16,1])
-Band18 = np.where(alti3DArray >=bins[18],Band17, alti3DArray+fs*parametrication.iloc[17,1])
-Band19 = np.where(alti3DArray >=bins[19],Band18, alti3DArray+fs*parametrication.iloc[18,1])
-Band20 = np.where(alti3DArray >= bins[20],Band19, alti3DArray+fs*parametrication.iloc[19,1])
-Band21= np.where(alti3DArray >= bins[21],Band20, alti3DArray+fs*parametrication.iloc[20,1])
-Band22= np.where(alti3DArray >= bins[22],Band21, alti3DArray+fs*parametrication.iloc[21,1])
-Band23= np.where(alti3DArray >= bins[23],Band22, alti3DArray+fs*parametrication.iloc[22,1])
-Band24= np.where(alti3DArray >= bins[24],Band23, alti3DArray+fs*parametrication.iloc[23,1])
-Band25= np.where(alti3DArray >= bins[25],Band24, alti3DArray+fs*parametrication.iloc[24,1])
-Band26= np.where(alti3DArray >= bins[26],Band25, alti3DArray+fs*parametrication.iloc[25,1])
-Band27= np.where(alti3DArray >= bins[27],Band26, alti3DArray+fs*parametrication.iloc[26,1])
-Band28= np.where(alti3DArray >= bins[28],Band27, alti3DArray+fs*parametrication.iloc[27,1])
-Band29= np.where(alti3DArray >= bins[29],Band28, alti3DArray+fs*parametrication.iloc[28,1])
-Band30= np.where(alti3DArray >= bins[30],Band29, alti3DArray+fs*parametrication.iloc[29,1])
-Band31= np.where(alti3DArray >= bins[31],Band30, alti3DArray+fs*parametrication.iloc[30,1])
-Band32= np.where(alti3DArray >= bins[32],Band31, alti3DArray+fs*parametrication.iloc[31,1])
-Band33= np.where(alti3DArray >= bins[33],Band32, alti3DArray+fs*parametrication.iloc[32,1])
-Alti_H1 = np.where(alti3DArray <= bins[34],Band33, alti3DArray+fs*parametrication.iloc[33,1])'''''
-
+#####Update alti 3d array band by band with dh and compare with glacier bed!!!!!
 
 Alti_H1 = None
 for index in range(len(parametrication)):
@@ -238,29 +171,18 @@ Alti_H1_Final = np.where(Alti_H1 >= edit_glacier_bed_array, Alti_H1, np.nan)
 print(Alti_H1_Final)
 
 
-#Difference
-
-substract_1 = alti3DArray-Alti_H1_Final
-
-
-
-########################################################Calculae new fs according to alti_H1_final#########################################################################
+########################################################Calculae new fs_H1 according to alti_H1_final#########################################################################
 
 #Mask alti_H1_Final
 
 masked_alti_H1= np.ma.masked_invalid(Alti_H1_Final)
-
-
 
 #Elevation Bands
 bins = list (parametrication.iloc[:,4])
 
 bins.append(parametrication.iloc[-1,5])
 
-
-
-
-#Hypsometrie
+#Hypsometry
 
 hypsometry, bins = np.histogram(masked_alti_H1.compressed(), bins=bins)
 
@@ -273,8 +195,7 @@ for i in hypsometry:
 
 fs_H1 = (BA* 997 * 2442977) / (((parametrication.iloc[:,2] * band_area_h1).sum())* 920)
 
-
-#Alti_H2
+#Calculate Alti_H2 with fs_H1
 Alti_H2 = None
 for index in range(len(parametrication)):
 
@@ -284,7 +205,7 @@ Alti_H2_float = np.array(Alti_H2, dtype= float)
 Alti_H2_Final = np.where(Alti_H2_float >= edit_glacier_bed_array, Alti_H2_float, np.nan)
 print(Alti_H2_Final)
 
-
+#########################################Calculate fs_H2 according to alti_H2################################################################
 #Mask alti_H2_Final
 
 masked_alti_H2= np.ma.masked_invalid(Alti_H2_Final)
@@ -315,7 +236,7 @@ Alti_List.append(Alti_H2_Final)
 #Indicate the number of years to project into the future
 years= int(input ('number of years you want to model in to the future:'))
 
-
+#Run the loop for as many years as insertet
 
 for x in range(years-2):
 
@@ -345,15 +266,22 @@ for x in range(years-2):
 
     fs_years = (BA * 997 * 2442977) / (((parametrication.iloc[:, 2] * band_area).sum()) * 920)
 
-
-#Safe the updated glacier surface
+#Safe the updated glacier surface to ws
 
 NewFile = ws+'/'+f'gltsf_{2016+years}.tif'
 createRasterFromCopy(NewFile, edit_alti3d, Alti_List[years-1])
 
 NewFile=None
-Alti_List=None
 
+print('you will find a .tif file: gltsf_... with the year number according to your insertet number of years in your ws ')
 
+#Calculating the difference in glacier surface elevation between 2016 and the year you have chosen
+substract_years = alti3DArray - Alti_List[years-1]
+NewFile = ws+'/'+f'substract_gltsf_2016-{2016+years}.tif'
+createRasterFromCopy(NewFile, edit_alti3d, substract_years)
 
-print('you will find a .tif file: gltsf_... with the year number according to your insertet number of years ')
+Alti_List=NewFile=None
+
+print('you will find a .tif file: substract_gltsf_2016-(the year you have chosen) in your ws')
+
+################################################################END#########################################################################
